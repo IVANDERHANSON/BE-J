d01be12e763b9381da5f090eb6636caa747f1c4c
hansonivander <hansonivander@gmail.com>
2023-12-03 21:40:01 +0700

    pertemuan3

--- pertemuan3/resources/views/home.blade.php
+++ pertemuan3/resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shoe Store</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 </head>
 <body>
@@ -13,11 +13,15 @@
             <a href=""><img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/shoe-store-logo-design-template-da7acb3f6546068cbc2272814166e8a4_screen.jpg?ts=1635317713" alt=""></a>
         </div>
         <div class="right">
-            <a href="">Home</a>
-            <a href="">Product</a>
+            <a href="#">Home</a>
+            <a href="/product">Product</a>
             <a href="">Contact Us</a>
             <a href="" class="login">Login</a>
         </div>
     </nav>
+
+    <div class="image">
+        <img src="{{ asset('asset/Store.jpg') }}" alt="">
+    </div>
 </body>
 </html>
